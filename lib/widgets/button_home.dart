454fd31f51fb1bde01c2501911c836db4b50a6b7
erdemgocen44 <erdemgocen44@gmail.com>
2022-07-09 23:12:07 +0200

    cupertino button start

---
+++ lib/widgets/button_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ButtonHome extends StatefulWidget {
+  const ButtonHome({Key? key}) : super(key: key);
+
+  @override
+  State<ButtonHome> createState() => _ButtonHomeState();
+}
+
+class _ButtonHomeState extends State<ButtonHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
