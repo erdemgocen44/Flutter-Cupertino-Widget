feabe1acd20bc92703aedf98c32b189a81f2d0f9
erdemgocen44 <erdemgocen44@gmail.com>
2022-08-11 22:09:24 +0200

    cupertino SearchTextField starting

---
+++ lib/widgets/search_text_field_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class SearchTextFieldHome extends StatefulWidget {
+  const SearchTextFieldHome({Key? key}) : super(key: key);
+
+  @override
+  State<SearchTextFieldHome> createState() => _SearchTextFieldHomeState();
+}
+
+class _SearchTextFieldHomeState extends State<SearchTextFieldHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
