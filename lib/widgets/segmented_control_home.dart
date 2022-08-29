766aa87607ee8ee4cf665e4f3c030b38400a6910
Erdem <erdemgocen44@gmail.com>
2022-08-29 22:18:25 +0200

    segmented controlled starting

---
+++ lib/widgets/segmented_control_home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SegmentedControlledHome extends StatefulWidget {
+  const SegmentedControlledHome({Key? key}) : super(key: key);
+
+  @override
+  State<SegmentedControlledHome> createState() =>
+      _SegmentedControlledHomeState();
+}
+
+class _SegmentedControlledHomeState extends State<SegmentedControlledHome> {
+  String? _currentText;
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
