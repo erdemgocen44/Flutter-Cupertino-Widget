4e2ca5efb0a569bb448b3164779974757ee1657e
Erdem <erdemgocen44@gmail.com>
2022-09-01 09:15:55 +0200

    slider starting

---
+++ lib/widgets/slider_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SliderHome extends StatefulWidget {
+  const SliderHome({Key? key}) : super(key: key);
+
+  @override
+  State<SliderHome> createState() => _SliderHomeState();
+}
+
+class _SliderHomeState extends State<SliderHome> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(child: child)
+  }
+}
