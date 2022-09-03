2887cc8b62b96653fc85ecadf130310918c80996
Erdem <erdemgocen44@gmail.com>
2022-09-03 22:53:08 +0200

    slider segmented controller starting

---
+++ lib/widgets/slider_segmented_controller_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class SliderSegmentedControllerHome extends StatefulWidget {
+  const SliderSegmentedControllerHome({Key? key}) : super(key: key);
+
+  @override
+  State<SliderSegmentedControllerHome> createState() => _SliderSegmentedControllerHomeState();
+}
+
+class _SliderSegmentedControllerHomeState extends State<SliderSegmentedControllerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    )
+  }
+}
