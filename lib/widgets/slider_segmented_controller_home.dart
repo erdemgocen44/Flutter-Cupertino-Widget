2b2444a2edff0c5c6e88488d262896d9083bac79
Erdem <erdemgocen44@gmail.com>
2022-09-03 22:58:56 +0200

    slider segmented controller starting

--- lib/widgets/slider_segmented_controller_home.dart
+++ lib/widgets/slider_segmented_controller_home.dart
@@ -6,23 +6,30 @@ class SliderSegmentedControllerHome extends StatefulWidget {
   const SliderSegmentedControllerHome({Key? key}) : super(key: key);
 
   @override
-  State<SliderSegmentedControllerHome> createState() => _SliderSegmentedControllerHomeState();
+  State<SliderSegmentedControllerHome> createState() =>
+      _SliderSegmentedControllerHomeState();
 }
 
-class _SliderSegmentedControllerHomeState extends State<SliderSegmentedControllerHome> {
-  int? _sliding=0;
+class _SliderSegmentedControllerHomeState
+    extends State<SliderSegmentedControllerHome> {
+  int? _sliding = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CupertinoSlidingSegmentedControl(children: const {
-          0:Text('Erdem'),1:Text('GÖÇEN'),2:Text('Flutter'),
-        },groupValue: _sliding, onValueChanged: (int? newValue){
-          setState(() {
-            _sliding=newValue;
-          });
-        }),
+        child: CupertinoSlidingSegmentedControl(
+            children: const {
+              0: Text('Erdem'),
+              1: Text('GÖÇEN'),
+              2: Text('Flutter'),
+            },
+            groupValue: _sliding,
+            onValueChanged: (int? newValue) {
+              setState(() {
+                _sliding = newValue;
+              });
+            }),
       ),
-    )
+    );
   }
 }
