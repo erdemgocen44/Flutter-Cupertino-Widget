59955385986fc35b2d627519b5399a4e9fb4872b
Erdem <erdemgocen44@gmail.com>
2022-09-08 23:08:52 +0200

    cupertino swith starting

---
+++ lib/widgets/switch_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SwitchHome extends StatefulWidget {
+  const SwitchHome({Key? key}) : super(key: key);
+
+  @override
+  State<SwitchHome> createState() => _SwitchHomeState();
+}
+
+class _SwitchHomeState extends State<SwitchHome> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
