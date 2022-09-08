7765558879e10c4dde5cebb758587f58faaa06e8
Erdem <erdemgocen44@gmail.com>
2022-09-08 23:36:24 +0200

    cupertino tab finir

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cupertino_widgets/widgets/home_page.dart';
+import 'package:flutter_cupertino_widgets/core/home_page.dart';
 import 'package:flutter_cupertino_widgets/core/rotes.dart';
 
 void main() {
