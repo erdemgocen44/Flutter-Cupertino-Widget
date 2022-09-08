9cea7a7fe4735762a2ff6cea64bd56751df15820
Erdem <erdemgocen44@gmail.com>
2022-09-08 23:19:50 +0200

    cupertino tab scaffold

--- lib/core/rotes.dart
+++ lib/core/rotes.dart
@@ -16,6 +16,7 @@ import 'package:flutter_cupertino_widgets/widgets/slider_home.dart';
 import 'package:flutter_cupertino_widgets/widgets/slider_segmented_controller_home.dart';
 import 'package:flutter_cupertino_widgets/widgets/starting_page.dart';
 import 'package:flutter_cupertino_widgets/widgets/switch_home.dart';
+import 'package:flutter_cupertino_widgets/widgets/tab_scaffold_home.dart';
 
 var rotalar = <String, WidgetBuilder>{
   "/scaffold": (context) => const StartingPage(),
@@ -35,4 +36,5 @@ var rotalar = <String, WidgetBuilder>{
   "/slider": (context) => const SliderHome(),
   "/slidersegment": (context) => const SliderSegmentedControllerHome(),
   "/switch": (context) => const SwitchHome(),
+  "/tab": (context) => const TabScaffoldHome(),
 };
