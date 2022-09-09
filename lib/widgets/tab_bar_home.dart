0ebf3657786214d8ba3780823122cc6c44e10674
Erdem <erdemgocen44@gmail.com>
2022-09-09 20:15:19 +0200

    cupertino tab view starting

--- lib/widgets/tab_bar_home.dart
+++ lib/widgets/tab_bar_home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/material.dart';
-
 class TabBarHome extends StatefulWidget {
   const TabBarHome({Key? key}) : super(key: key);
 
---
+++ lib/widgets/tab_view_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class TabViewHome extends StatefulWidget {
+  const TabViewHome({Key? key}) : super(key: key);
+
+  @override
+  State<TabViewHome> createState() => _TabViewHomeState();
+}
+
+class _TabViewHomeState extends State<TabViewHome> {
+  @override
+  Widget build(BuildContext context) {}
+}
