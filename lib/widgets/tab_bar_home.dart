eaca6a0c5b8d9220ac87ecfb7f0ed3cea0b47851
Erdem <erdemgocen44@gmail.com>
2022-09-09 12:54:42 +0200

    cupertino tabBar starting

---
+++ lib/widgets/tab_bar_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class TabBarHome extends StatefulWidget {
+  const TabBarHome({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarHome> createState() => _TabBarHomeState();
+}
+
+class _TabBarHomeState extends State<TabBarHome> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
